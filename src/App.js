52ff2cee2ad7fab06f0f8dab3b640444165bd6ee
Jdure <Jdure067@gmail.com>
2020-01-20 22:54:53 -0500

    navigation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home } from './Home';
+import { About } from './About';
+import { Contact } from './Contact';
+import { NoMatch } from './NoMatch';
+import { Layout } from './components/Layout';
+import { NavigationBar } from './components/NavigationBar';
 
-function App() {
+class App extends Component {
+  render() {
   return (
    <React.Fragment>
+   <NavigationBar />
+   <Layout>
    <Router>
     <Switch>
-      <Route path="/" component={Home} />
+      <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/contact" component={Contact} />
       <Route component={NoMatch} />
     </Switch>
     </Router>
+    </Layout>
    </React.Fragment>
   );
+}
 }
 
 export default App;
